4a13cf81a16f20729ddec33642c820b89832a2f6
Enes Kahraman <official.phoique@gmail.com>
2020-02-06 22:18:05 +0300

    Update error.ts

--- src/controllers/error.ts
+++ src/controllers/error.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
 function indexError(request: Request, response: Response, next: NextFunction) {
-
-  console.log(response.status(404));
   return response.status(404).render('error/404');
 }
 
